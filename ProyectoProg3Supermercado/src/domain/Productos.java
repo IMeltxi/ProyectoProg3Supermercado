1010eed7480bd4d934d9e7041505b958a098852d
i.melchisidor <i.melchisidor@10.166.56.190>
2025-10-15 12:50:14 +0200

    Clase Productos

---
+++ ProyectoProg3Supermercado/src/domain/Productos.java
@@ -0,0 +1,45 @@
+package domain;
+
+public class Productos {
+	private String nombre;
+	private String descripcion;
+	private float precio;
+	private int stock;
+	
+	public Productos() {	
+	}
+	
+	public Productos(String nombre, String descripcion, float precio, int stock) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.stock = stock;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+}
